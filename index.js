7512b7d5f1e0015086a4365c147b4113bb2b430b
Jason Leach <jlspursfan@gmail.com>
2019-05-01 17:10:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 // Add your functions and code here
 
-function destructivelyAppendKitten (kittens) {
-  kittens.appendKitten()
-}
+var destructivelyAppendKitten = kittens.append()
+
+var destructivelyPrependKitten = kittens.
+
+var destructivelyRemoveLastKitten = kittens.pop()
+
+var destructivelyRemoveFirstKitten = kittens.unshift()
+
+var appendKitten = 
+
+var prependKitten = 
+
+var removeLastKitten = 
+
+var removeFirstKitten = 
